f069320d8e4f8cd46edf9d40d050499d63a79d9b
Osama92 <try2rym@yahoo.com>
2024-08-30 12:08:55 +0100

    adding elements

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { useFonts } from 'expo-font';
+
 
 export default function Index() {
+  const [loaded] = useFonts({
+    Montserrat: require('../assets/fonts/Montserrat.ttf'),
+  });
+
+  
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  text: {
+    fontFamily: 'Montserrat',
+    fontSize: 16,
+  }
+});
