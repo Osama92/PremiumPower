d7b0c1fa3e1392e4a225f5087c191c15d61d452a
Osama92 <try2rym@yahoo.com>
2024-08-30 12:54:39 +0100

    Updating

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,24 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 import { useFonts } from 'expo-font';
 
 
 export default function Index() {
   const [loaded] = useFonts({
     Montserrat: require('../assets/fonts/Montserrat.ttf'),
+    MontserratBold: require('../assets/fonts/Montserrat-ExtraBold.ttf')
   });
 
   
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      <View style={styles.LogoSection}>
+      <Image source={require('../assets/images/PPS.png')} style={styles.logosize} resizeMode="contain"/>
+      </View>
+      <View style={styles.editArea}>
+        <Text style={styles.regularText}>Welcome Back</Text>
+        <Text style={styles.bigText}>LOGIN</Text>
+      </View>
     </View>
   );
 }
@@ -20,12 +27,35 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    justifyContent: 'center',
     backgroundColor: '#fff',
     alignItems: 'center',
   },
-  text: {
+  regularText: {
     fontFamily: 'Montserrat',
     fontSize: 16,
+    paddingLeft: 15,
+    paddingTop: 20
+  },
+  bigText: {
+    fontFamily: 'MontserratBold',
+    fontSize: 40,
+    paddingLeft: 15,
+  },
+  LogoSection: {
+    width: '100%',
+    height: '10%',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    marginTop:50
+  },
+  logosize: {
+    height: '100%',
+    width: '30%',
+  },
+  editArea: {
+    width: '100%',
+    height: '80%',
+    backgroundColor: 'orange'
   }
+
 });
