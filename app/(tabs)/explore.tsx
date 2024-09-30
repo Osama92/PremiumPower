7eeb2fe46aeac5155dbc76b67092e84fa81941b6
Osama92 <try2rym@yahoo.com>
2024-09-30 16:46:37 +0100

    Commit

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -5,7 +5,7 @@ export default class explore extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>explore</Text>
+        <Text>explore Bawo</Text>
       </View>
     )
   }
